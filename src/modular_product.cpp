b6038ed63ff8491ab57f47d3fbb9f56d603cc15c
ekatwikz <ekatwikz@gmail.com>
2023-11-19 22:18:49 +0100

    Slightly better doc for the dotlang switch

--- src/modular_product.cpp
+++ src/modular_product.cpp
@@ -20,13 +20,14 @@ using std::span;
  * @param argc should be >=2
  * @param argv should have the filename to read at [1] and [2]\n
  * if either are "-", stdin will be read instead for that one\n
+ * if [3] is "dot", it'll convert the output to DOT language
  *
  * @return 0, or 1 for parse errors
  */
 auto main(int argc, char* argv[]) -> int {
     auto args = span(argv, static_cast<size_t>(argc));
     if (argc < 3) {
-        cerr << "Usage: " << args[0] << " <filename1> <filename2>\n";
+        cerr << "Usage: " << args[0] << " <filename1> <filename2> [dot]\n";
         return 1;
     }
 
